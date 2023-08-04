658549fb22cda01584bb8ae1b8b753b26035b952
MyatKaungKyaw <myatkaungg31@gmail.com>
2023-08-04 18:28:34 +0900

    add state

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Note {
+  id: number;
+  content: string;
 }
 
-export default App;
+const App = () => {
+  const [newNote, setNewNote] = useState('');
+  const [notes, setNotes] = useState<Note[]>([{ id: 1, content: 'testing' }]);
+
+  useEffect(() => {
+    axios.get<Note[]>('http://localhost:3001/notes').then((response) => {
+      setNotes(response.data);
+    });
+  }, []);
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
